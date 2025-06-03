9c84f55b8129ce9efbbfedc6b95c6f3bb504da95
Terry Rice <trice7454@gmail.com>
2025-06-03 17:01:05 -0500

    Added blog content and images for the static site, including posts on Glorfindel, Tom Bombadil, and "The Lord of the Rings," along with a contact page and supporting images. Removed the old test case `test_whole_index_md`.

--- src/test_main.py
+++ src/test_main.py
@@ -432,60 +432,6 @@ the **same** even with inline stuff
             "<div><ul><li><a href=\"https://en.wikipedia.org/wiki/Markdown\">What is Markdown?</a></li><li><a href=\"https://en.wikipedia.org/wiki/HTML\">What is HTML?</a></li></ul></div>",
         )
         
-    def test_whole_index_md(self):
-        md = """
-# Tolkien Fan Club
-
-![JRR Tolkien sitting](/images/tolkien.png)
-
-Here's the deal, **I like Tolkien**.
-
-> "I am in fact a Hobbit in all but size."
->
-> -- J.R.R. Tolkien
-
-## Blog posts
-
-- [Why Glorfindel is More Impressive than Legolas](/blog/glorfindel)
-- [Why Tom Bombadil Was a Mistake](/blog/tom)
-- [The Unparalleled Majesty of "The Lord of the Rings"](/blog/majesty)
-
-## Reasons I like Tolkien
-
-- You can spend years studying the legendarium and still not understand its depths
-- It can be enjoyed by children and adults alike
-- Disney _didn't ruin it_ (okay, but Amazon might have)
-- It created an entirely new genre of fantasy
-
-## My favorite characters (in order)
-
-1. Gandalf
-2. Bilbo
-3. Sam
-4. Glorfindel
-5. Galadriel
-6. Elrond
-7. Thorin
-8. Sauron
-9. Aragorn
-
-Here's what `elflang` looks like (the perfect coding language):
-
-```
-func main(){
-    fmt.Println("Aiya, Ambar!")
-}
-```
-
-Want to get in touch? [Contact me here](/contact).
-
-This site was generated with a custom-built [static site generator](https://www.boot.dev/courses/build-static-site-generator-python) from the course on [Boot.dev](https://www.boot.dev).        
-        """
-        node = markdown_to_html_node(md)
-        html = node.to_html()
-        self.assertTrue(html.startswith("<div><h1>Tolkien Fan Club</h1><p><img alt=\"JRR Tolkien sitting\" src=\"/images/tolkien.png\">"))
-        
-
     def test_extract_title(self):
         md = """
 # This is a heading
